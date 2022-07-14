d83ffb1c71aadbd5eed02d3c4e16f94944d45669
safwan917 <safwan917@gmail.com>
2022-07-14 10:28:45 +0800

    updated some css problems

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ const navBar = document.querySelector(".primary-navigation");
 
 hamburgerBtn.addEventListener("click", () => {
   hamburgerBtn.classList.toggle("is-active");
-  navBar.classList.toggle("is-enabled");
-
   const nav = document.querySelector(".primary-navigation");
   const body = document.querySelector("body");
 
@@ -13,12 +11,31 @@ hamburgerBtn.addEventListener("click", () => {
     nav.style.visibility = "visible";
     nav.style.opacity = "1";
 
+    nav.classList.add("is-enabled");
     body.classList.add("menu-open");
   } else {
     nav.style.height = "0";
     nav.style.visibility = "hidden";
     nav.style.opacity = "0";
 
+    nav.classList.remove("is-enabled");
     body.classList.remove("menu-open");
+  }
+});
+
+window.addEventListener("resize", () => {
+  const nav = document.querySelector(".primary-navigation");
+  if (window.innerWidth >= "900") {
+    nav.style.height = "fit-content";
+    nav.style.opacity = "1";
+    nav.style.visibility = "visible";
+
+    nav.classList.remove("is-enabled");
+
+    hamburgerBtn.classList.remove("is-active");
+  } else {
+    nav.style.height = "0";
+    nav.style.opacity = "0";
+    nav.style.visibility = "hidden";
   }
 });
